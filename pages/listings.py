7cbbd849fe241aed058cafe8aa55aff73c34762b
AtharvaN88 <atharvanilapwar88@gmail.com>
2025-04-17 00:07:02 -0400

    connected to local db

--- pages/listings.py
+++ pages/listings.py
@@ -2,6 +2,7 @@ import streamlit as st
 from geopy.geocoders import Nominatim
 import json
 import pandas as pd
+import mysql.connector
 
 
 geolocator = Nominatim(user_agent="listings-coordinates")
@@ -9,11 +10,43 @@ geolocator = Nominatim(user_agent="listings-coordinates")
 file = open('sample-json/test_data.json', 'r', encoding='utf-8').read()
 data = json.loads(file)
 
+db_config = {
+    "host": "localhost/IP",
+    "user": "username",
+    "password": "password",
+    "database": "db-name"
+}
+
+def get_db_connection():
+    conn = mysql.connector.connect(**db_config)
+    return conn
+
 def get_map_coordindates(listing):
     address = f"{listing['address']}, {listing['city']}, {listing['state']}"
     location = geolocator.geocode(address)
 
     return [location.latitude, location.longitude]
+
+
+def fetch_listings_from_db():
+    conn = get_db_connection()
+    query = """
+    SELECT 
+        id, address, city, state, zip, rent, bedrooms, bathrooms, appliances, images, seller_name, seller_contact
+    FROM listings;
+    """
+    df = pd.read_sql(query, conn)
+    conn.close()
+
+    df['appliances'] = df['appliances'].apply(lambda x: x.split(','))
+    df['images'] = df['images'].apply(lambda x: x.split(','))
+
+    df['seller'] = df.apply(lambda row: {'name': row['seller_name'], 'contact': row['seller_contact']}, axis=1)
+    df = df.drop(columns=['seller_name', 'seller_contact'])
+
+    return df
+
+df = fetch_listings_from_db()
 
 coordinates = [get_map_coordindates(x) for x in data]
 df_coordinates = pd.DataFrame(coordinates, columns=['latitude', 'longitude'])
